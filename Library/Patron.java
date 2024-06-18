52f3575fe7b398b148ac1047043e2aff3273e882
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2024-06-18 06:48:22 -0400

    class patron implemented

---
+++ Library/Patron.java
@@ -0,0 +1,39 @@
+package Library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patron {
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public Patron(String name) {
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    @Override
+    public String toString() {
+        return "Patron{" +
+                "name='" + name + '\'' +
+                ", borrowedBooks=" + borrowedBooks +
+                '}';
+    }
+}
+
